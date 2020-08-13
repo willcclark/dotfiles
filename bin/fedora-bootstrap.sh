93573071432164bab51ae6cb471a5cf194a167e9
willcclark <67292935+willcclark@users.noreply.github.com>
2020-08-13 15:53:32 -0400

    Update fedora-bootstrap.sh

--- bin/fedora-bootstrap.sh
+++ bin/fedora-bootstrap.sh
@@ -65,6 +65,13 @@ sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/maste
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
 
 ###
+# Brave Browser
+###
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+
+###
 # Force update the whole system to the latest and greatest
 ###
 
@@ -79,7 +86,9 @@ sudo dnf distro-sync -y
 dnf=(
 alacritty # fastest Terminal
 codium
+dnf-plugins-core
 wget 
+steam
 curl 
 ruby 
 ruby-devel 
